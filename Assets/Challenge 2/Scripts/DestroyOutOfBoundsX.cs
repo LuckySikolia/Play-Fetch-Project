6f7698a124b75c81a2fd6713617ec45836feb892
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-07 02:32:31 +0300

    destroy game object off screen
    Fix game object moving outside environment boundary

--- Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -4,21 +4,25 @@ using UnityEngine;
 
 public class DestroyOutOfBoundsX : MonoBehaviour
 {
-    private float leftLimit = 30;
-    private float bottomLimit = -5;
+    private float leftLimit = -35f;
+    private float bottomLimit = -1f;
 
     // Update is called once per frame
     void Update()
     {
-        // Destroy dogs if x position less than left limit
-        if (transform.position.x > leftLimit)
+        // Destroy dogs if x position less than the left limit
+        if (transform.position.x < leftLimit)
         {
+            //Debug.Log("GONE BEYOND LEFT LIMIT");
             Destroy(gameObject);
+            
         } 
-        // Destroy balls if y position is less than bottomLimit
-        else if (transform.position.z < bottomLimit)
+            // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.y < bottomLimit)
         {
+            //Debug.Log("GONE BEYOND BOTTOM LIMIT");
             Destroy(gameObject);
+            
         }
 
     }

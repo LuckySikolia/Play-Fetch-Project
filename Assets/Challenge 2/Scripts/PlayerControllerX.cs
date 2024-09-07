61eb76b763c52771a5e79d0008208291a9d49eaa
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2024-09-07 03:11:08 +0300

    Fix spam spacebar
    add 0.5 seconds between dog spawning

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,33 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    private float spawnCooldown = 0.5f;
+    private float timeSinceLastSpawn;
+
     // Update is called once per frame
     void Update()
     {
-        // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        //code to deal with the spaming of the spacebar key
+        //increase the timer
+        timeSinceLastSpawn += Time.deltaTime;
+
+        //check if the space bar is pressed and cooldown has passed
+        if(Input.GetKeyDown(KeyCode.Space) && timeSinceLastSpawn >= spawnCooldown)
         {
+            //spawn the dog
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            //reset the timer to 0 to start the cooldown period again
+            timeSinceLastSpawn = 0f;
         }
+
+
+
+
+        // On spacebar press, send dog
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        //}
     }
 }
